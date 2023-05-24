6fe3c9156851d3bfb10bb088d2e6e22bd8b54a7a
keidyya <kya5@njit.edu>
2023-05-23 20:31:09 -0400

    html, css, and js codes

---
+++ index.js
@@ -0,0 +1,5 @@
+const taskInput = document.querySelector(".task-input input"),
+filters = document.querySelectorAll(".filter span"),
+cleared = document.querySelector(".clearbtn"),
+taskbox = document.querySelector(".taskbox");
+
